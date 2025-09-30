2c05e2a15308b2f2b00bbdf38e80562a35bfd452
Tony Brantley <82984236+TBrantl@users.noreply.github.com>
2025-09-30 03:12:52 -0400

    Update PolyzRNG.lua

--- PolyzRNG.lua
+++ PolyzRNG.lua
@@ -82,7 +82,7 @@ CombatTab:CreateToggle({
                                 local torso = zombie:FindFirstChild("Torso") or zombie:FindFirstChild("UpperTorso")
                                 if humanoid and humanoid.Health > 0 and head and torso then
                                     local dist = (head.Position - playerPos).Magnitude
-                                    if dist < minDist and dist < 100 then -- Max range 100 studs
+                                    if dist < minDist and dist < 500 then -- Max range 100 studs
                                         -- 360-degree LOS check using raycast
                                         local rayParams = RaycastParams.new()
                                         rayParams.FilterDescendantsInstances = {player.Character}
