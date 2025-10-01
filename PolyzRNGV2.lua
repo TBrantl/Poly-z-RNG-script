084877f94daca4e2894fc2079edf8739f54c0d63
Tony Brantley <82984236+TBrantl@users.noreply.github.com>
2025-10-01 00:31:09 -0400

    Update PolyzRNGV2.lua

--- PolyzRNGV2.lua
+++ PolyzRNGV2.lua
@@ -237,9 +237,9 @@ MiscTab:CreateButton({
         }  
 
         for _, attr in ipairs(enchants) do  
-            if vars:GetAttribute(attr) ~= nil then
+            if vars:GetAttribute(attr) ~= nil then  
                 vars:SetAttribute(attr, true)  
-            end
+            end  
         end
         Rayfield:Notify({
             Title = "Enhancement",
@@ -257,10 +257,10 @@ MiscTab:CreateButton({
         local gunData = player:FindFirstChild("GunData")
         if not gunData then return end
 
-        for _, value in ipairs(gunData:GetChildren()) do
-            if value:IsA("StringValue") then
+        for _, value in ipairs(gunData:GetChildren()) do  
+            if value:IsA("StringValue") then  
                 value.Value = "celestial"  
-            end
+            end  
         end
         Rayfield:Notify({
             Title = "Weapons",
@@ -354,13 +354,13 @@ OpenTab:CreateToggle({
 local autoOpenPet = false
 OpenTab:CreateToggle({
     Name = "🐾 Pet Crates",
-        CurrentValue = false,
-        Callback = function(state)
+    CurrentValue = false,
+    Callback = function(state)
         autoOpenPet = state
-            if state then
-                task.spawn(function()
+        if state then
+            task.spawn(function()
                 while autoOpenPet do
-                            pcall(function()
+                    pcall(function()
                         for i = 1, selectedQuantity do
                             ReplicatedStorage.Remotes.OpenPetCrate:InvokeServer(1)
                             task.wait(0.1)
@@ -389,12 +389,12 @@ OpenTab:CreateToggle({
                             task.wait(0.1)
                         end
                     end)
-                        task.wait(0.1)
-                    end
-                end)
-            end
+                    task.wait(0.1)
+                end
+            end)
         end
-    })
+    end
+})
 
 
 -- Mod Tab
